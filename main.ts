d97dc9fca04e89168a5bc42c5f32ca5c6d797846
MleSTAC <78829273+MleSTAC@users.noreply.github.com>
2021-02-22 09:01:44 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,24 +2,24 @@ let Lights_On = false
 input.onSound(DetectedSound.Loud, function () {
     Lights_On = !(Lights_On)
     if (Lights_On) {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # # . # .
+            # . . . #
+            # # # # #
+            `)
         music.playMelody("G E G - G E G - ", 120)
         music.playMelody("A G F E D E F E ", 120)
-        music.playMelody("G C - C C C - C ", 120)
+        music.playMelody("E F G C C C C C ", 120)
         music.playMelody("D E F G - G D D ", 120)
         music.playMelody("F E D C C C C C ", 120)
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            # . . . #
-            . . . . .
-            . # # # .
-            `)
     } else {
         basic.showLeds(`
-            . . . . .
+            . # . . .
             # # . # #
-            # # . # #
-            . . . . .
+            # # . . #
+            . . . . #
             . # # # .
             `)
     }
